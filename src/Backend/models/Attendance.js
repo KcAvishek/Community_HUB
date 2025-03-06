09a4554b14ebfff1381089f71109624240d7572f
Mr.Abhishek <kcabhisekh@gmail.com>
2025-03-06 20:40:51 +0545

    update

--- src/Backend/models/Attendance.js
+++ src/Backend/models/Attendance.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const attendanceSchema = new mongoose.Schema({
   community_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Community', required: true },
-  meeting_date: { type: Date, default: Date.now }, // Date of the meeting
-  attendees: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // List of attendees
+  meeting_date: { type: Date, default: Date.now }, 
+  attendees: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], 
 });
 
 module.exports = mongoose.model('Attendance', attendanceSchema);
